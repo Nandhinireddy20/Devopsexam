558c3d05baf9b30d1ee268b5e9ed8ed4ca8690dc
Nandhinireddy20 <nandhinireddy2035@gmail.com>
2025-09-02 14:12:10 +0530

    registration from created

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, redirect, url_for
+app = Flask(__name__)
+@app.route('/', methods=['GET', 'POST'])
+def home():
+	if request.method == 'POST':
+		print("Form submitted successfully!")
+		print("Form Data:", request.form)
+		print("New feature")
+		return "Registration successful!"
+	return render_template('form.html')
+if __name__ == '__main__':
+	app.run(debug=True)
